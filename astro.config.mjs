069cc7dfd1873f4c829b0d7bd7524fa2a864a3ab
Andrei Shershukov <andrei.shershukov@bluelions.ee>
2025-04-05 00:18:59 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: process.env.HOST_URL,
+  base: process.env.HOST_BASE_PATH,
+});
